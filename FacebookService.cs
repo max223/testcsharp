214f2ffeccbf664a6ecc9d5b28d025b936de8f5c
Maxime Rogissart <m.rogissart@gmail.com>
2017-11-15 19:04:27 +0100

    push pour debug

---
+++ FacebookService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AREA
+{
+    public class FacebookService
+    {
+        private readonly IFacebookWrapper _facebookClient;
+        private FacebookWrapper facebookWrapper;
+
+        public FacebookService(IFacebookWrapper facebookClient)
+        {
+            _facebookClient = facebookClient;
+        }
+
+        public FacebookService(FacebookWrapper facebookWrapper)
+        {
+            this.facebookWrapper = facebookWrapper;
+        }
+
+        public async Task PostOnWallAsync(string accessToken, string message)
+        {
+            await _facebookClient.PostAsync(accessToken, "me/feed", new { message });
+
+        }
+    }
+}
+
---
+++ HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AREA.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //TRY FACEBOOK WRAPPER
+            var accessToken = "ACEESS_TOKEN_DE_JOZZ";
+            var facebookWrapper = new FacebookWrapper();
+            var facebookService = new FacebookService(facebookWrapper);
+
+            var postOnWallTask = facebookService.PostOnWallAsync(accessToken, "Hello from C# .NET Core! SISI L'AREA");
+            //Task.WaitAll(postOnWallTask); si je le remet, ça attend vraiment ma task et ça plante, si je l'enleve ça plante pas mais ma requete n'ai pas faites
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
