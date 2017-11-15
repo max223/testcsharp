32d7a24e54582b2f32a3d0ad17e2cd312534523a
Maxime Rogissart <m.rogissart@gmail.com>
2017-11-15 19:13:59 +0100

    put your handz up for jozé

---
+++ AREA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AREA.MvcApplication" Language="C#" %>
---
+++ AREA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AREA.Startup))]
+namespace AREA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
